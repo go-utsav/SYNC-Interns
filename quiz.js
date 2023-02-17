f5821369b9f3f57e62373f8a972de771aa20881b
go-utsav <utsavgohel3@gmail.com>
2023-02-17 20:33:17 +0530

    .

--- quiz.js
+++ quiz.js
@@ -1,104 +1,101 @@
 //Question bank
-var questionBank= [
+var questionBank = [
     {
-        question : 'What should be the first tag in any HTML document?',
-        option : ['html','document','head','title'],
-        answer : 'html'
+        question: 'what is the extension of html file',
+        option: ['.js', '.css', '.xml', '.html'],
+        answer: '.html'
     },
     {
-        question : 'How can you make a bulleted list?',
-        option : ['list','nl','ul','ol'],
-        answer : 'ul'
+        question: 'How many types of CSS are there ?',
+        option: ['In-line', 'Internal', 'External', 'all of above'],
+        answer: 'all of above'
     },
     {
-        question : 'How many parts does a HTML character entity has',
-        option : ['Character entities have as many parts as is necessary','A character entity has three parts','A character entity has two parts',' Character entities only have one part'],
-        answer : 'A character entity has three parts'
+        question: 'How many parts does a HTML character entity has',
+        option: ['Character entities have as many parts as is necessary', 'A character entity has three parts', 'A character entity has two parts', ' Character entities only have one part'],
+        answer: 'A character entity has three parts'
     },
     {
-        question : 'Which is not a domain name extension',
-        option : [' .com','org','mil','.int'],
-        answer : '.int'
+        question: 'Which is not a domain name extension',
+        option: [' .com', 'org', 'mil', '.int'],
+        answer: '.int'
     },
     {
-        question : 'Which of the following are commonly found on web pages?',
-        option : ['internet','hyperlinks','intranet','all of the above'],
-        answer : 'hyperlinks'
+        question: 'Which of the following are commonly found on web pages?',
+        option: ['internet', 'hyperlinks', 'intranet', 'all of the above'],
+        answer: 'hyperlinks'
     }
 ]
 
-var question= document.getElementById('question');
-var quizContainer= document.getElementById('quiz-container');
-var scorecard= document.getElementById('scorecard');
-var option0= document.getElementById('option0');
-var option1= document.getElementById('option1');
-var option2= document.getElementById('option2');
-var option3= document.getElementById('option3');
-var next= document.querySelector('.next');
-var points= document.getElementById('score');
-var span= document.querySelectorAll('span');
-var i=0;
-var score= 0;
+var question = document.getElementById('question');
+var quizContainer = document.getElementById('quiz-container');
+var scorecard = document.getElementById('scorecard');
+var option0 = document.getElementById('option0');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
+var next = document.querySelector('.next');
+var points = document.getElementById('score');
+var span = document.querySelectorAll('span');
+var i = 0;
+var score = 0;
 
 //function to display questions
-function displayQuestion(){
-    for(var a=0;a<span.length;a++){
-        span[a].style.background='none';
+function displayQuestion() {
+    for (var a = 0; a < span.length; a++) {
+        span[a].style.background = 'none';
     }
-    question.innerHTML= 'Q.'+(i+1)+' '+questionBank[i].question;
-    option0.innerHTML= questionBank[i].option[0];
-    option1.innerHTML= questionBank[i].option[1];
-    option2.innerHTML= questionBank[i].option[2];
-    option3.innerHTML= questionBank[i].option[3];
-    stat.innerHTML= "Question"+' '+(i+1)+' '+'of'+' '+questionBank.length;
+    question.innerHTML = 'Q.' + (i + 1) + ' ' + questionBank[i].question;
+    option0.innerHTML = questionBank[i].option[0];
+    option1.innerHTML = questionBank[i].option[1];
+    option2.innerHTML = questionBank[i].option[2];
+    option3.innerHTML = questionBank[i].option[3];
+    stat.innerHTML = "Question" + ' ' + (i + 1) + ' ' + 'of' + ' ' + questionBank.length;
 }
 
 //function to calculate scores
-function calcScore(e){
-    if(e.innerHTML===questionBank[i].answer && score<questionBank.length)
-    {
-        score= score+1;
-        document.getElementById(e.id).style.background= 'limegreen';
+function calcScore(e) {
+    if (e.innerHTML === questionBank[i].answer && score < questionBank.length) {
+        score = score + 1;
+        document.getElementById(e.id).style.background = 'limegreen';
     }
-    else{
-        document.getElementById(e.id).style.background= 'tomato';
+    else {
+        document.getElementById(e.id).style.background = 'tomato';
     }
-    setTimeout(nextQuestion,300);
+    setTimeout(nextQuestion, 300);
 }
 
 //function to display next question
-function nextQuestion(){
-    if(i<questionBank.length-1)
-    {
-        i=i+1;
+function nextQuestion() {
+    if (i < questionBank.length - 1) {
+        i = i + 1;
         displayQuestion();
     }
-    else{
-        points.innerHTML= score+ '/'+ questionBank.length;
-        quizContainer.style.display= 'none';
-        scoreboard.style.display= 'block'
+    else {
+        points.innerHTML = score + '/' + questionBank.length;
+        quizContainer.style.display = 'none';
+        scoreboard.style.display = 'block'
     }
 }
 
 //click events to next button
-next.addEventListener('click',nextQuestion);
+next.addEventListener('click', nextQuestion);
 
 //Back to Quiz button event
-function backToQuiz(){
+function backToQuiz() {
     location.reload();
 }
 
 
 //function to check Answers
-function checkAnswer(){
-    var answerBank= document.getElementById('answerBank');
-    var answers= document.getElementById('answers');
-    answerBank.style.display= 'block';
-    scoreboard.style.display= 'none';
-    for(var a=0;a<questionBank.length;a++)
-    {
-        var list= document.createElement('li');
-        list.innerHTML= questionBank[a].answer;
+function checkAnswer() {
+    var answerBank = document.getElementById('answerBank');
+    var answers = document.getElementById('answers');
+    answerBank.style.display = 'block';
+    scoreboard.style.display = 'none';
+    for (var a = 0; a < questionBank.length; a++) {
+        var list = document.createElement('li');
+        list.innerHTML = questionBank[a].answer;
         answers.appendChild(list);
     }
 }
@@ -114,7 +111,7 @@ let quizTime = quizTimeInMinutes / 60;
 let counting = document.getElementById("count-down");
 
 function startCountdown() {
-    let quizTimer = setInterval(function() {
+    let quizTimer = setInterval(function () {
         if (quizTime <= 0) {
             clearInterval(quizTimer);
             showScores();
